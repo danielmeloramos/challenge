e2e21091514d0f7508e5e25b59a1ce17d231eb98
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2021-07-20 11:24:09 -0300

    Criado logica do customer balancing para rodas os testes e passarem os mesmos.

---
+++ customer-success-balancing.js
@@ -0,0 +1,160 @@
+/**
+ * Returns the id of the CustomerSuccess with the most customers
+ * @param {array} customerSuccess
+ * @param {array} customers
+ * @param {array} customerSuccessAway
+ */
+function customerSuccessBalancing(
+  customerSuccess,
+  customers,
+  customerSuccessAway
+) {
+  /**
+   * ===============================================
+   * =========== Write your solution here ==========
+   * ===============================================
+   */
+
+  //1- Atualizar gerentes somente com os disponiveis
+  customerSuccess = customerSuccess.filter(val => !customerSuccessAway.includes(val.id));
+
+  //2- Ordenar os gerentes success pelo nível dele
+  customerSuccess.sort((a, b) => (a.score > b.score) ? 1 : -1);
+
+  //3- Verifica se score do ultimo gerente é o mesmo do ultimo cliente
+  var customerSuccessVerify = customerSuccess[customerSuccess.length - 1];
+  var customerLast = customers[customers.length - 1];
+  if(customerSuccessVerify.score === customerLast.score)
+    return customerSuccessVerify.id;
+
+  //4- Percorrer todos os clientes encontrar o primeiro com o maior ou igual score, adicionar contador
+  customers.forEach(function(customer) {
+    const customerSuccessFound = customerSuccess.find(element => element.score >= customer.score);
+    if (customerSuccessFound) {
+      if (customerSuccessFound.counter === undefined)
+        customerSuccessFound.counter = 0;
+      
+      customerSuccessFound.counter += 1;
+    }
+  });
+
+  //5- Filtrar todos os gerentes que tem contador e ordernar pelo contador
+  const compareObjects = (a, b) => (a.counter > b.counter) ? 1 : -1;
+  customerSuccess = customerSuccess.filter(key => key.counter !== undefined).sort(compareObjects);
+
+  //6- Verificar se encontrou algum gerente, se não encontrar retornar empate
+  if(customerSuccess.length === 0)
+    return 0;
+    
+  //7- Obter o penultimo gerente (se existir) e o último gerente  
+  const customerSuccessPenultimate = customerSuccess[customerSuccess.length - 2];
+  const customerSuccessLast = customerSuccess[customerSuccess.length - 1];
+
+  //8- Verificar se houve empate entre o peultimo e ultimo gerente
+  var tieValue = customerSuccessPenultimate !== undefined && customerSuccessPenultimate.counter === customerSuccessLast.counter;
+  
+  //9- Se houver empate, retornar 0, caso contrário retornar o ID do último gerente com Score maior
+  return tieValue ? 0 : customerSuccessLast.id;
+}
+
+
+test("Scenario 1", () => {
+  const css = [
+    { id: 1, score: 60 },
+    { id: 2, score: 20 },
+    { id: 3, score: 95 },
+    { id: 4, score: 75 },
+  ];
+  const customers = [
+    { id: 1, score: 90 },
+    { id: 2, score: 20 },
+    { id: 3, score: 70 },
+    { id: 4, score: 40 },
+    { id: 5, score: 60 },
+    { id: 6, score: 10 },
+  ];
+  const csAway = [2, 4];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
+}); 
+
+
+function buildSizeEntities(size, score) {
+  const result = [];
+  for (let i = 0; i < size; i += 1) {
+    result.push({ id: i + 1, score });
+  }
+  return result;
+}
+
+function mapEntities(arr) {
+  return arr.map((item, index) => ({
+    id: index + 1,
+    score: item,
+  }));
+}
+
+function arraySeq(count, startAt){
+  return Array.apply(0, Array(count)).map((it, index) => index + startAt)
+}
+
+
+test("Scenario 2", () => {
+  const css = mapEntities([11, 21, 31, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+
+test("Scenario 3", () => {
+  const testTimeoutInMs = 100;
+  const testStartTime = new Date().getTime();
+
+  const css = mapEntities(arraySeq(999, 1))
+  const customers = buildSizeEntities(10000, 998);
+  const csAway = [999];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(998);
+
+  if (new Date().getTime() - testStartTime > testTimeoutInMs) {
+    throw new Error(`Test took longer than ${testTimeoutInMs}ms!`);
+  }
+});
+
+
+test("Scenario 4", () => {
+  const css = mapEntities([1, 2, 3, 4, 5, 6]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+
+test("Scenario 5", () => {
+  const css = mapEntities([100, 2, 3, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
+});
+
+
+test("Scenario 6", () => {
+  const css = mapEntities([100, 99, 88, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [1, 3, 2];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+
+test("Scenario 7", () => {
+  const css = mapEntities([100, 99, 88, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [4, 5, 6];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(3);
+});  
